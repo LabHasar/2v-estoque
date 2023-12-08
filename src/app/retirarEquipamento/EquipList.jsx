f99854ab4331c47e3b24b196c5eb7c6eac9b6faf
LabHasar <brlabhasar1@gmail.com>
2023-12-08 14:59:40 -0300

    comunicação com banco

--- src/app/retirarEquipamento/EquipList.jsx
+++ src/app/retirarEquipamento/EquipList.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import { collection, getDocs } from "firebase/firestore";
+import { onSnapshot, collection } from 'firebase/firestore';
 import { db } from "../configdb/firebase";
 import sx from "./styles.module.css";
 import Checkbox from '@mui/material/Checkbox';
@@ -8,21 +8,23 @@ import Checkbox from '@mui/material/Checkbox';
 import { Box, Divider } from "@mui/material";
 
 export default function EquipList() {
-  // const [itens, setItens] = useState([]);
+  const [itens, setItens] = useState([]);
 
-  // useEffect(() => {
-  //   const unsubscribe = onSnapshot(collection(db, "item"), (querySnapshot) => {
-  //     const dados = querySnapshot.docs.map((doc) => ({
-  //       id: doc.id,
-  //       ...doc.data(),
-  //     }));
-  //     setItens(dados);
-  //   });
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(db, "item"), (querySnapshot) => {
+      const dados = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setItens(dados);
+    });
 
-  //   // Função de cleanup ao desmontar o componente
-  //   return () => unsubscribe();
-  // }, []); // O array vazio como segundo argumento garante que o useEffect só seja executado uma vez, ao montar o componente
+    // Função de cleanup ao desmontar o componente
+    return () => unsubscribe();
+  }, []); // O array vazio como segundo argumento garante que o useEffect só seja executado uma vez, ao montar o componente
 
+
+console.log("itens aqui - ", itens.nome);
   return (
     <Box>
       {/* Renderiza os itens, você pode criar um componente específico para isso se desejar */}
@@ -37,18 +39,12 @@ export default function EquipList() {
         </div>
 
         <div className="text-black font-semibold mb-4 flex flex-col" style={{ maxHeight: '300px', overflowY: 'auto' }}>
-          {/* {itens.map((item) => (
+          {itens.map((item) => (
             <div key={item.id} className={sx.list}>
               <p>{item.nome}</p>
-            </div>
-          ))} */}
-          {Array.from({ length: 10 }).map((_, index) => (
-            <div key={`fake-item-${index}`} className={sx.list}>
-              <p>Item de Teste {index + 1}</p>
               <Checkbox/>
             </div>
           ))}
-          
         </div>
         
       </div>
--- src/app/retirarEquipamento/page.jsx
+++ src/app/retirarEquipamento/page.jsx
@@ -1,10 +1,11 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Box, Container } from "@mui/material";
 import Header from "../components/header/page";
 import sx from "./styles.module.css";
 import Button from '@mui/material/Button';
 import EquipList from "./equipList";
+
 
 export default function RetirarEquipamentoPage() {
   return (
